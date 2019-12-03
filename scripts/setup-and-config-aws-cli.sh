fcdb1443c900895fa78044fb4740bb167aacf6db
burib <burib@users.noreply.github.com>
2019-12-03 22:47:56 +0100

    add approval step to deploy to production

--- scripts/setup-and-config-aws-cli.sh
+++ scripts/setup-and-config-aws-cli.sh
@@ -1,22 +1,43 @@
 #!/usr/bin/env bash
-set -e
+set -eou pipefail
 
-sudo apt update
-sudo apt install -y awscli
+set +u
+if [ -z $IS_PRODUCTION ]; then
+  IS_PRODUCTION=false
+fi
+set -u
 
 if [[ "$IS_PRODUCTION" == true ]]; then
-  export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID_PROD
-  export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY_PROD
-  export AWS_PROFILE_NAME=$AWS_PROFILE_NAME_PROD
-  export AWS_STAGE_NAME=$AWS_STAGE_NAME_PROD
+  export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID_PROD:-}
+  export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY_PROD:-}
+  export AWS_PROFILE_NAME=${AWS_PROFILE_NAME_PROD-default}
+  export AWS_STAGE_NAME=${AWS_STAGE_NAME_PROD-prod}
+elif [[ "$IS_MASTER_BRANCH" == true ]]; then
+  export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID_STAGING:-}
+  export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY_STAGING:-}
+  export AWS_PROFILE_NAME=${AWS_PROFILE_NAME_STAGING-default}
+  export AWS_STAGE_NAME=${AWS_STAGE_NAME_STAGING-staging}
 else
-  export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID_DEV
-  export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY_DEV
-  export AWS_PROFILE_NAME=$AWS_PROFILE_NAME_DEV
-  export AWS_STAGE_NAME=$AWS_STAGE_NAME_DEV # TODO: maybe different stage per PR opener ?
+  export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID_DEV:-}
+  export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY_DEV:-}
+  export AWS_PROFILE_NAME=${AWS_PROFILE_NAME_DEV-default}
+  # default deployment stage is dev
+  # if you want to have it deployed to your custom stage create a variable in circleCI project Environment Variables page
+  # https://circleci.com/gh/USERNAME/PROJECT/edit#env-vars
+  # and create a variable called AWS_STAGE_NAME_DEV with the value of $CIRCLE_USERNAME or $CIRCLE_PR_USERNAME
+  # CIRCLE_USERNAME	    = The GitHub or Bitbucket username of the user who triggered the build.
+  # CIRCLE_PR_USERNAME	= The GitHub or Bitbucket username of the user who created the pull request. Only available on forked PRs.
+  export AWS_STAGE_NAME="$(echo ${AWS_STAGE_NAME_DEV-dev})"
 fi
-aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID --profile $AWS_PROFILE_NAME
-aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY_PROD --profile $AWS_PROFILE_NAME
-echo "Exporting variables to \"$BASH_ENV\" file."
-echo -e "export AWS_PROFILE_NAME=${AWS_PROFILE_NAME}" >> "$BASH_ENV"
-echo -e "export AWS_STAGE_NAME=${AWS_STAGE_NAME}" >> "$BASH_ENV"
+
+if [[ -z "$AWS_ACCESS_KEY_ID" || -z "$AWS_SECRET_ACCESS_KEY" ]]; then
+  echo "AWS_ACCESS_KEY_ID or AWS_SECRET_ACCESS_KEY is not set in circleCI environment variables."
+else
+  aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID --profile $AWS_PROFILE_NAME
+  aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY_PROD --profile $AWS_PROFILE_NAME
+  echo "Exporting variables to \"$BASH_ENV\" file."
+  echo -e "export AWS_PROFILE_NAME=${AWS_PROFILE_NAME}" >> "$BASH_ENV"
+  echo -e "export AWS_STAGE_NAME=${AWS_STAGE_NAME}" >> "$BASH_ENV"
+fi
+
+
--- scripts/setup-context-variables.sh
+++ scripts/setup-context-variables.sh
@@ -1,33 +1,13 @@
 #!/usr/bin/env bash
-set -e
-
-version_tag_pattern="v[0-9]+(\.[0-9]+)*"
-if [[ $CIRCLE_TAG =~ $version_tag_pattern ]]; then
-  export HAS_RELEASE_TAG=true
-else
-  export HAS_RELEASE_TAG=false
-fi
+set -eou pipefail
 
 CURRENT_BRANCH=$(git symbolic-ref HEAD | cut -d"/" -f3)
 export CURRENT_HASH=$(git rev-parse HEAD)
 export PRIOR_HASH=$(git log -1 --pretty=format:"%H" --no-merges)
 
-if [[ "${CURRENT_BRANCH}" == "master" ]]; then
-    export IS_MASTER_BRANCH=true
-else
-    export IS_MASTER_BRANCH=false
-fi
-
-if $IS_MASTER_BRANCH && $HAS_RELEASE_TAG ; then
-  export IS_PRODUCTION=true
-else
-  export IS_PRODUCTION=false
-fi
-
-if $IS_MASTER_BRANCH && !($HAS_RELEASE_TAG) ; then
-  export IS_STAGING=true
-else
-  export IS_STAGING=false
+set +u
+if [ -z $IS_PRODUCTION ]; then
+  IS_PRODUCTION=false
 fi
 
 if [ -z "$CI_PULL_REQUEST" ]
@@ -36,38 +16,38 @@ then
 else
   export IS_PULL_REQUEST=true
 fi
+set -u
+
+if [[ "${CURRENT_BRANCH}" == "master" ]]; then
+    export IS_MASTER_BRANCH=true
+else
+    export IS_MASTER_BRANCH=false
+fi
 
 export STAGE="dev" # default stage is dev environment                           ->  dev.example.com
 if $IS_PRODUCTION ; then
-  export STAGE="prod" # master + release tag goes to production environment     ->  example.com
-fi
-if $IS_STAGING ; then
-  export STAGE="staging" # master + no release tag goes to staging environment  ->  staging.examle.com
-fi
-if $IS_PULL_REQUEST ; then
+  export STAGE="prod" # if IS_PRODUCTION variable is set, prod environment      ->  example.com
+elif $IS_MASTER_BRANCH ; then
+  export STAGE="staging" # master goes to staging environment at first          ->  staging.example.com
+elif $IS_PULL_REQUEST ; then
   export STAGE=$CIRCLE_USERNAME # pull request goes to PR opener's environment  ->  username.example.com
 fi
 
 mkdir ~/workspace
 touch ~/workspace/new-env-vars
-echo -e "export HAS_RELEASE_TAG=${HAS_RELEASE_TAG}" >> ~/workspace/new-env-vars
 echo -e "export CURRENT_BRANCH=${CURRENT_BRANCH}" >> ~/workspace/new-env-vars
 echo -e "export IS_MASTER_BRANCH=${IS_MASTER_BRANCH}" >> ~/workspace/new-env-vars
 echo -e "export IS_PULL_REQUEST=${IS_PULL_REQUEST}" >> ~/workspace/new-env-vars
-echo -e "export IS_PRODUCTION=${IS_PRODUCTION}" >> ~/workspace/new-env-vars
 echo -e "export STAGE=${STAGE}" >> ~/workspace/new-env-vars
 echo -e "export CURRENT_HASH=${CURRENT_HASH}" >> ~/workspace/new-env-vars
 echo -e "export PRIOR_HASH=${PRIOR_HASH}" >> ~/workspace/new-env-vars
 
 echo "Setting 'CURRENT_HASH' variable to: ${CURRENT_HASH}"
 echo "Setting 'PRIOR_HASH' variable to: ${PRIOR_HASH}"
-echo "Setting 'HAS_RELEASE_TAG' variable to: ${HAS_RELEASE_TAG}"
 echo "Setting 'CURRENT_BRANCH' variable to: ${CURRENT_BRANCH}"
 echo "Setting 'IS_MASTER_BRANCH' variable to: ${IS_MASTER_BRANCH}"
-echo "Setting 'IS_PRODUCTION' variable to: ${IS_PRODUCTION}"
-echo "Setting 'IS_STAGING' variable to: ${IS_STAGING}"
 echo "Setting 'IS_PULL_REQUEST' variable to: ${IS_PULL_REQUEST}"
 echo "Setting 'STAGE' variable to: ${STAGE}"
 
 cat ~/workspace/new-env-vars >> $BASH_ENV
-echo "Exporting variables to \"$BASH_ENV\" file, so we can use the variable in other jobs as well."
+echo "Exporting variables to \"$BASH_ENV\" file, so we can use them in other jobs as well."
